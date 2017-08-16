a7c28615eaed02583cfa8594af86bd983ff52af3
Oberon <360119124@qq.com>
2017-08-16 21:49:55 +0800

    Updated

--- Game1/GameObjects/GameUnit.cs
+++ Game1/GameObjects/GameUnit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Game1.GameObjects
 {
@@ -11,6 +12,68 @@ namespace Game1.GameObjects
         private Party party1;
 
         public virtual Party Party1 { get => party1; set => party1 = value; }
+
+        private float hp;
+        private float pp;
+        private float exp;
+        private float lv;
+        private float atk;
+        private float def;
+        private float spd;
+        public float hpMax;
+        public float ppMax;
+        public float expMax;
+        public float lvMax;
+        public float atkStd;
+        public float defStd;
+        public float spdStd;
+
+        
+        
+        public float Hp
+        {
+            get
+            {
+                return MathHelper.Clamp(hp, 0, hpMax);
+            }
+            set
+            {
+                hp = MathHelper.Clamp(value, 0, hpMax);
+            }
+        }
+        public float Pp
+        {
+            get
+            {
+                return MathHelper.Clamp(pp, 0, ppMax);
+            }
+            set
+            {
+                pp = MathHelper.Clamp(value, 0, ppMax);
+            }
+        }
+        public float Exp
+        {
+            get
+            {
+                return MathHelper.Clamp(exp, 0, expMax);
+            }
+            set
+            {
+                exp = MathHelper.Clamp(value, 0, expMax);
+            }
+        }
+        public float Lv
+        {
+            get
+            {
+                return MathHelper.Clamp(lv, 0, lvMax);
+            }
+            set
+            {
+                exp = MathHelper.Clamp(value, 0, lvMax);
+            }
+        }
 
         public virtual void LoadContent()
         {
